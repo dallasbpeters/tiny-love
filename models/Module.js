22957f342e66c693b4df3282889a21cea42155e0
Dallas Peters <dallaspeters@gmail.com>
2015-07-06 19:52:54 -0500

    sortable
    @mark-bradshaw I’ve added a bit to make these sortable in the admin.
    can you have a peek and make sure I didn’t miss anything with this?
    It’s working in the admin but wasn’t sure if anything else needed to be
    updated.

--- models/Module.js
+++ models/Module.js
@@ -1,7 +1,8 @@
 var keystone = require('keystone');
 
 var Module = new keystone.List('Module', {
-	autokey: { from: 'name', path: 'key', unique: true }
+	autokey: { from: 'name', path: 'key', unique: true, },
+	sortable: true
 });
 
 Module.add({
