3dc2b0e809003772773ea0d765c492f3d3c40a61
Mark Bradshaw <mark.bradshaw@gmail.com>
2015-07-07 21:00:03 -0500

    module page listing wired up

--- routes/views/user/module.js
+++ routes/views/user/module.js
@@ -9,6 +9,20 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'module';
 
+    view.on('init', function(next) {
+
+        // Load the modules by sortOrder
+    	keystone.list('Module').model.findOne({ key: req.params.module }).exec(function(err, result) {
+            locals.module = result;
+
+            // Load the modules by sortOrder
+        	keystone.list('Lesson').model.find({ module: locals.module.id }).exec(function(err, results) {
+                locals.lessons = results;
+                next(err);
+            });
+        });
+    });
+
 	// Render the view
 	view.render('user/module');
 
