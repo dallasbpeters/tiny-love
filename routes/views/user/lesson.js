de71ad61db728d0ffa5ac477deee38ef842a4cd7
Mark Bradshaw <mark.bradshaw@gmail.com>
2015-07-07 21:10:56 -0500

    lesson page wired up

--- routes/views/user/lesson.js
+++ routes/views/user/lesson.js
@@ -9,6 +9,19 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'lesson';
 
+    view.on('init', function(next) {
+
+        // Load the modules by sortOrder
+    	keystone.list('Module').model.findOne({ key: req.params.module }).exec(function(err, result) {
+            locals.module = result;
+
+            keystone.list('Lesson').model.findOne({ key: req.params.lesson }).exec(function(err, result) {
+                locals.lesson = result;
+                next(err);
+            });
+        });
+    });
+
 	// Render the view
 	view.render('user/lesson');
 
