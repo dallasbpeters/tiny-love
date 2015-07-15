5002c2ef0276ad2afd4591f862830cc1e579f235
Dallas Peters <dallaspeters@gmail.com>
2015-07-15 10:56:13 -0500

    error cleanup

--- models/Module.js
+++ models/Module.js
@@ -1,7 +1,7 @@
 var keystone = require('keystone');
 
 var Module = new keystone.List('Module', {
-	autokey: { from: 'name', path: 'key', unique: true, },
+	autokey: { from: 'name', path: 'key', unique: true },
 	sortable: true
 });
 
--- routes/middleware.js
+++ routes/middleware.js
@@ -40,24 +40,24 @@ exports.initLocals = function(req, res, next) {
 
 
 exports.initErrorHandlers = function(req, res, next) {
-    
+
     res.err = function(err, title, message) {
         res.status(500).render('errors/500', {
             err: err,
             errorTitle: title,
             errorMsg: message
         });
-    }
-    
+    };
+
     res.notfound = function(title, message) {
         res.status(404).render('errors/404', {
             errorTitle: title,
             errorMsg: message
         });
-    }
-    
+    };
+
     next();
-    
+
 };
 
 
