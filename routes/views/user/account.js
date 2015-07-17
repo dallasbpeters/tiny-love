9c5c6ad7124687fbd9f504e17987f0f051b3bdc5
Mark Bradshaw <mark.bradshaw@gmail.com>
2015-07-16 21:04:52 -0500

    change password functioning

--- routes/views/user/account.js
+++ routes/views/user/account.js
@@ -9,7 +9,29 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'account';
 
+    view.on('post', { action: 'changePassword' }, function(next) {
+
+        if (req.body.newpass === '') {
+            req.flash('error', 'Please enter a new password.');
+            return next();
+        }
+
+        keystone.list('User').model.findOne({_id: req.user._id}).exec(function(err, user) {
+
+            user.password = req.body.newpass;
+            user.save(function(err) {
+
+                if (err) {
+					console.log('update password error', err);
+					req.flash('error', 'There was a problem updating your password.  Please contact us for assistance.');
+					return next();
+				}
+
+                res.redirect('/user/dashboard');
+            });
+        });
+    });
+
 	// Render the view
 	view.render('user/account');
-
 };
