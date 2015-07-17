155df15358525705d5b6551b3b3dffbb049ae47f
Mark Bradshaw <mark.bradshaw@gmail.com>
2015-07-16 19:26:26 -0500

    add completed/uncompleted class to lessons on module page

--- routes/views/user/module.js
+++ routes/views/user/module.js
@@ -9,21 +9,44 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'module';
 
+	view.on('init', function(next) {
+
+        keystone.list('User').model.findOne({_id: req.user._id}).exec(function(err, result) {
+
+            locals.currentUser = result;
+            next(err);
+        });
+    });
+
     view.on('init', function(next) {
 
         // Load the modules by sortOrder
     	keystone.list('Module').model.findOne({ key: req.params.module }).exec(function(err, result) {
             locals.module = result;
 
-            // Load the modules by sortOrder
-        	keystone.list('Lesson').model.find({ module: locals.module.id }).exec(function(err, results) {
-                locals.lessons = results;
-                next(err);
-            });
+            next(err);
+        });
+    });
+
+	view.on('init', function(next) {
+
+        // Load the lessons by sortOrder
+    	keystone.list('Lesson').model.find({ module: locals.module.id }).exec(function(err, results) {
+            locals.lessons = results;
+
+			for (var i = 0; i < locals.lessons.length; ++i) {
+				if (locals.currentUser.completedLessons.indexOf(locals.lessons[i]._id.toString()) !== -1) {
+					locals.lessons[i].status = 'completed';
+				}
+				else {
+					locals.lessons[i].status = 'uncompleted';
+				}
+			}
+
+            next(err);
         });
     });
 
 	// Render the view
 	view.render('user/module');
-
 };
