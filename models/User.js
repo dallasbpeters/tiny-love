dd8be86113382251f58c08659b9305aa4fbe463e
Mark Bradshaw <mark.bradshaw@gmail.com>
2015-08-02 21:48:13 -0500

    remove Post model relationship

--- models/User.js
+++ models/User.js
@@ -30,7 +30,7 @@ User.schema.virtual('canAccessKeystone').get(function() {
  * Relationships
  */
 
-User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+// User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
 
 
 /**
